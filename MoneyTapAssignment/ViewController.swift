785cf55562ba733a17bb21654a1d4e2a192c4dd4
shailendra.suriyal <shailendra.suriyal@intimetec.com>
2018-07-28 19:03:44 +0530

    Initial Commit

---
+++ MoneyTapAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneyTapAssignment
+//
+//  Created by Shailendra Suriyal on 28/07/18.
+//  Copyright © 2018 Shailendra Suriyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
